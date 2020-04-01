4af4762e89845d22458bdf309bec64785d6e982e
Wei Xin Koh <e0201350@u.nus.edu>
2020-04-01 17:25:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,11 @@ function keepGoing(array, changeValue, skipValue) {
 
   return array;
 }
+
+function findBy(array, findFn) {
+  for (let i = 0; i < array.length; i++) {
+    if (findFn(array[i])) {
+      return array[i];
+    }
+  }
+}
