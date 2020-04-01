36262630ab4cca2d21c519076a4776c9738992a6
Wei Xin Koh <e0201350@u.nus.edu>
2020-04-01 17:09:10 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function breakOut(array, changeValue, stopValue) {
+  array.forEach(element => {
+    if (element === stopValue) {
+      break;
+    }
+    
+    element = changeValue;
+  })
+
+  return array;
+}
